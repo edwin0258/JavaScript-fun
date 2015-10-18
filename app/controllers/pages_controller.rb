97c8122ed02fafd683cec7e86313b63adc45fd56
edwin0258 <edwin0258@gmail.com>
2015-10-18 02:08:41 +0000

    Add post score button.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
     def home
     end
+    def create
+        @score = Score.create(score_params)
+    end
+    
+    private
+        def score_params
+            params.require(:score).permit(:total, :name)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root "pages#home"
+  root "pages#home" do
+    resources :scores
+    member do
+      get 'score'
+      post 'score'
+    end
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
